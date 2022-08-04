b8570dfd3995fef85a7232c1ad5ec17f3c8aa33e
Wrxuan <2450341686@qq.com>
2022-08-04 16:18:33 +0800

    完成

---
+++ assets/js/user/user_pwd.js
@@ -0,0 +1,30 @@
+//密码验证表单
+const form = layui.form;
+
+form.verify({
+    pwd: [/^[\S]{6,12}$/, "密码必须6到12位，且不能出现空格"],
+    samePwd: (val) => {
+        if (val === $("[name=oldPwd]").val()) return "新旧密码不能相同！";
+    },
+    rePwd: (val) => {
+        if (val !== $("[name=newPwd]").val()) return "两次密码不一致！";
+    },
+});
+
+const layer = layui.layer;
+
+//发送请求，重置密码
+$(".layui-form").on("submit", (e) => {
+  e.preventDefault();
+  $.ajax({
+    type: "POST",
+    url: "/my/updatepwd",
+    data: form.val('formPassword'),
+    success: (res) => {
+      const{status,message} =res
+      layer.msg(message)
+      if(status !==0)return
+      $(".layui-form")[0].reset();
+    },
+  });
+});
