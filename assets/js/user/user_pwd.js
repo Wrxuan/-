4800eb67298ace7df56de9567d99452d2f64e669
Wrxuan <2450341686@qq.com>
2022-08-04 16:29:36 +0800

    完成

--- assets/js/user/user_pwd.js
+++ assets/js/user/user_pwd.js
@@ -1,30 +1,29 @@
-//密码验证表单
-const form = layui.form;
-
+const form = layui.form
 form.verify({
-    pwd: [/^[\S]{6,12}$/, "密码必须6到12位，且不能出现空格"],
-    samePwd: (val) => {
-        if (val === $("[name=oldPwd]").val()) return "新旧密码不能相同！";
-    },
-    rePwd: (val) => {
-        if (val !== $("[name=newPwd]").val()) return "两次密码不一致！";
-    },
-});
+  pwd: [/^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'],
 
-const layer = layui.layer;
+  samePwd: (val) => {
+    if (val === $('[name=oldPwd]').val()) return '新旧密码不能相同！'
+  },
+  rePwd: (val) => {
+    if (val !== $('[name=newPwd]').val()) return '两次密码不一致！'
+  },
+})
 
-//发送请求，重置密码
-$(".layui-form").on("submit", (e) => {
-  e.preventDefault();
+$('.layui-form').submit(function (e) {
+  e.preventDefault()
   $.ajax({
-    type: "POST",
-    url: "/my/updatepwd",
+    type: 'POST',
+    url: '/my/updatepwd',
     data: form.val('formPassword'),
     success: (res) => {
-      const{status,message} =res
+      console.log(res)
+      const { status, message } = res
       layer.msg(message)
-      if(status !==0)return
-      $(".layui-form")[0].reset();
+      if (status !== 0) return
+      // 重置表单
+      $('.layui-form')[0].reset()
     },
-  });
-});
+  })
+})
+
